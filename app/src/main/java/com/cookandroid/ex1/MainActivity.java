ce323919ca7603c22cd6bef58f76fc49ceab48f5
smitehead <dkdldjdy12@naver.com>
2025-02-23 22:19:19 +0900

    version 0.1 : 기본토대 제작(수정한것들 전부 적어주세요)

--- app/src/main/java/com/cookandroid/ex1/MainActivity.java
+++ app/src/main/java/com/cookandroid/ex1/MainActivity.java
@@ -1,4 +1,4 @@
-package com.cookandroid.ex1;
+package com.cookandroid.ex1;//11
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
